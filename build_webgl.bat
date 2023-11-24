44a445bd1c5a27a964ae81a9dde76bc298b233ac
Uladzimir Milanovich <vladimir.milanovich@gmail.com>
2023-11-24 19:17:20 +0100

    Change script format from bat to sh

--- build_webgl.bat
+++
@@ -1,9 +0,0 @@
-set logpath=".\Logs\Build-WebGL.log"
-set unitypath="C:\Program Files\Unity\Hub\Editor\2022.3.9f1\Editor\Unity.exe"
-set buildpath=".\Builds\WebGL"
-set method="ProjectBuilder.BuildWebGL"
-
-del %logpath%
-rmdir %buildpath%
-
-%unitypath% -quit -batchmode -executeMethod %method% -logFile %logpath% -nographics
